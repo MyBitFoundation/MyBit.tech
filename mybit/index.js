c315cf7b9dd29e79c4bf4da5ea2fed1d7745011e
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-12-12 16:05:33 -0600

    Restructured endpoints to function more like isolated endpoints for now.sh according to v2

--- mybit/index.js
+++ mybit/index.js
@@ -1,38 +1,7 @@
-const app = require('express')(),
-      fs = require('fs'),
-      path = require('path'),
-      { promisify } = require('util');
+const app = require('express')()
 
-const readFileAsync = promisify(fs.readFile);
+app.get('*', (req, res) => {
+  res.send('MyBit API Endpoint');
+})
 
-async function getVersion() {
-  try {
-    const filePath = path.join(__dirname, 'version');
-    const text = await readFileAsync(
-      filePath,
-      { encoding: 'utf8' }
-    );
-    return text;
-  }
-  catch (err) {
-    console.log('Err', err)
-    return err;
-  }
-}
-
-function main(app, version) {
-  app.set('port', process.env.PORT || 8080);
-
-  app.get('/', (req, res) => {
-    res.send('MyBit General API');
-  })
-
-  app.get('/version', (req, res) => {
-    res.send(version);
-  })
-
-  app.listen(app.get('port'));
-}
-
-getVersion()
-  .then(version => main(app, version));
+app.listen(process.env.PORT || 8080);
