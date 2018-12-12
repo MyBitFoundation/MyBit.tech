cfb16fdb26fe208ccaf93f27ca866eb89d2295db
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-12-12 14:31:14 -0600

    Added ports and setup instructions locally

--- mybit-go/index.js
+++ mybit-go/index.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 
+app.set('port', process.env.PORT || 8081);
+
 app.get('*', (req, res) => {
   res.send('MyBit Go API');
 })
 
-app.listen();
+app.listen(app.get('port'));
--- mybit/index.js
+++ mybit/index.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 
+app.set('port', process.env.PORT || 8080);
+
 app.get('*', (req, res) => {
   res.send('MyBit General API');
 })
 
-app.listen();
+app.listen(app.get('port'));
