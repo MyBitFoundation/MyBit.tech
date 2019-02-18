2df754b75b51c9e3b59525ea89ea70696bdd1a25
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-02-18 03:55:56 +0000

    fixed identifiers

--- src/controllers/collateralController.js
+++ src/controllers/collateralController.js
@@ -9,23 +9,23 @@ import {
 const ADDRESS = process.env.ADDRESS;
 const ADDRESS_PRIVATE_KEY = Buffer.from(process.env.ADDRESS_PRIVATE_KEY, 'hex');
 
-const web3Collateral = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`));
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`));
 
 export const lockEscrow = async (assetId, assetManager, escrow) => {
   return new Promise(async (resolve, reject) => {
     try{
-      var txnCount = await web3Collateral.eth.getTransactionCount(ADDRESS);
+      const txnCount = await web3.eth.getTransactionCount(ADDRESS);
 
-      const assetCollateral = new web3Collateral.eth.Contract(
+      const assetCollateral = new web3.eth.Contract(
         AssetCollateral.ABI,
         AssetCollateral.ADDRESS
       );
 
-      var data = await assetCollateral.methods.lockEscrow(assetId, assetManager, escrow).encodeABI();
-      let rawTx = {
-        nonce: web3Collateral.utils.toHex(txnCount),
+      const data = await assetCollateral.methods.lockEscrow(assetId, assetManager, escrow).encodeABI();
+      const rawTx = {
+        nonce: web3.utils.toHex(txnCount),
         gasPrice: GAS_PRICE,
-        gasLimit: web3Collateral.utils.toHex(140000),
+        gasLimit: web3.utils.toHex(140000),
         to: AssetCollateral.ADDRESS,
         data: data,
       }
@@ -33,7 +33,7 @@ export const lockEscrow = async (assetId, assetManager, escrow) => {
       const tx = new Tx(rawTx)
       tx.sign(ADDRESS_PRIVATE_KEY)
       let serializedTx = "0x" + tx.serialize().toString('hex');
-      web3Collateral.eth.sendSignedTransaction(serializedTx)
+      web3.eth.sendSignedTransaction(serializedTx)
       .on('receipt', function (receipt) {
         resolve(200)
       }).on('error', function (error) {
